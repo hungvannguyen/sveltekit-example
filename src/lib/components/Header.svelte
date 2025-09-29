4af9a58e80135658dac6657fd9552b9b0bb5d729
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-09-29 10:06:38 +0700

    add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,58 @@
+<script>
+	import logoUrl from '$lib/assets/logo.png';
+</script>
+
+<header class="sticky top-0 z-50 w-full bg-black/80 text-[#7f7f7f] backdrop-blur-md">
+	<div class="mx-auto flex max-w-7xl items-center justify-between px-3 py-2 md:py-4 lg:px-6">
+		<a href="/">
+			<img src={logoUrl} alt="Logo" class="h-[32px] w-auto" />
+		</a>
+
+		<nav class="hidden flex-1 items-center justify-center gap-2 md:flex">
+			<div class="flex gap-2">
+				<a
+					href="/"
+					class="ring-offset-background focus-visible:ring-ring data-[active=true]:text-accent relative inline-flex cursor-pointer items-center justify-center gap-2 rounded-4xl px-3 py-1 font-medium tracking-normal whitespace-nowrap text-gray-500 transition-colors duration-300 hover:text-gray-300 focus:text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 dark:hover:text-white [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+				>
+					Courses
+				</a>
+				<a
+					href="/"
+					class="ring-offset-background focus-visible:ring-ring data-[active=true]:text-accent relative inline-flex cursor-pointer items-center justify-center gap-2 rounded-4xl px-3 py-1 font-medium tracking-normal whitespace-nowrap text-gray-500 transition-colors duration-300 hover:text-gray-300 focus:text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 dark:hover:text-white [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+				>
+					Learning Paths
+				</a>
+
+				<a
+					href="/"
+					class="ring-offset-background focus-visible:ring-ring data-[active=true]:text-accent relative inline-flex cursor-pointer items-center justify-center gap-2 rounded-4xl px-3 py-1 font-medium tracking-normal whitespace-nowrap text-gray-500 transition-colors duration-300 hover:text-gray-300 focus:text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 dark:hover:text-white [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+				>
+					Lifetime Access
+				</a>
+
+				<a
+					href="/"
+					class="ring-offset-background focus-visible:ring-ring data-[active=true]:text-accent relative inline-flex cursor-pointer items-center justify-center gap-2 rounded-4xl px-3 py-1 font-medium tracking-normal whitespace-nowrap text-gray-500 transition-colors duration-300 hover:text-gray-300 focus:text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 dark:hover:text-white [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+				>
+					Forum
+				</a>
+
+				<a
+					href="/"
+					class="ring-offset-background focus-visible:ring-ring data-[active=true]:text-accent relative inline-flex cursor-pointer items-center justify-center gap-2 rounded-4xl px-3 py-1 font-medium tracking-normal whitespace-nowrap text-gray-500 transition-colors duration-300 hover:text-gray-300 focus:text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 dark:hover:text-white [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+				>
+					Contact
+				</a>
+			</div>
+		</nav>
+
+		<a
+			href="/"
+			class="block rounded-3xl bg-white px-3 py-1 no-underline outline-none hover:bg-gray-300 hover:no-underline"
+		>
+			<div class="text-base tracking-normal text-black transition-colors duration-300">
+				Members Area
+			</div>
+		</a>
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import '../lib/styles/global.css';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
+
+<Header />
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children?.()}
+<Footer />
