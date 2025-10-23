de67b1440a595c08a2812d194e6590c1ea720157
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-10-23 09:51:40 +0700

    feat: add blog post layout with image and title for improved content presentation

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,28 @@
+<div class="mx-auto max-w-7xl px-3 py-12 lg:px-6">
+	<div class="mb-8">
+		<div class="relative h-[400px] w-full overflow-hidden rounded-lg">
+			<img
+				src="https://cms.firebits.top/assets/24ccfc7a-c4c9-4fc0-af5a-148d26c85aa5"
+				alt="XAUt0 is gold without borders: Q&amp;A with USDT0 co-founder Lorenzo R"
+				class="object-cover"
+				decoding="async"
+			/>
+		</div>
+	</div>
+
+	<h2
+		class="font-heading lg:text-headline mb-4 text-4xl font-normal
+         !text-[#fff] text-[#7f7f7f] md:text-5xl"
+	>
+		XAUt0 is gold without borders: Q&amp;A with USDT0 co-founder Lorenzo R
+	</h2>
+
+	<div
+		class="bg-border mb-8 h-[1px] w-full shrink-0"
+		id="bits-c3"
+		role="separator"
+		aria-orientation="horizontal"
+		data-orientation="horizontal"
+		data-separator-root=""
+	></div>
+</div>
